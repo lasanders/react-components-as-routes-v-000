ee40a1a51d7d2f0371a52dd18f0c7c412ff2d043
Lara Sanders <larasanders@gmail.com>
2019-05-27 15:32:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -40,9 +40,9 @@ const Login = () => {
 ReactDOM.render((
   <Router>
   <React.Fragment>
-      <Route path="/" render={() => <h1>Home!</h1>} />
-      <Route exact path="/about" render={About} />
-      <Route exact path="/login" render={Login} />
+  <Route path="/" component={Home} />
+  <Route exact path="/about" component={About} />
+  <Route exact path="/login" component={Login} />
     </React.Fragment>
   </Router>),
   document.getElementById('root')
